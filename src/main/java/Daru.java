6e06d9555c4c892e20844924cb054456a38d661c
Cywez <cherylchow04@gmail.com>
2026-08-25 11:14:12 +0800

    Added greetings

--- src/main/java/Daru.java
+++ src/main/java/Daru.java
@@ -1,10 +1,19 @@
 public class Daru {
+
+    private static final String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
         String banner = " ____      _     ____   _   _ \n"
                 + "|  _ \\    / \\   |  _ \\ | | | |\n"
                 + "| | | |  / _ \\  | |_) || | | |\n"
                 + "| |_| | / ___ \\ |  _ < | |_| |\n"
                 + "|____/ /_/   \\_\\|_| \\_\\ \\___/ \n";
+        System.out.println(LINE);
         System.out.println(banner);
+        System.out.println("Hello! I'm DaruDaru, you can call me Daru!");
+        System.out.println("What can I do for you today?");
+        System.out.println(LINE);
+        System.out.println("Bye bye! See you!");
+        System.out.println(LINE);
     }
 }
