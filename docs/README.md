44dc9311f49e88bbff2b8f14d84f4bcf537984c7
Cywez <cherylchow04@gmail.com>
2026-08-25 11:04:28 +0800

    Renamed chatbot from Duke to Daru

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Daru User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Daru.java
@@ -0,0 +1,10 @@
+public class Daru {
+    public static void main(String[] args) {
+        String banner = " ____      _     ____   _   _ \n"
+                + "|  _ \\    / \\   |  _ \\ | | | |\n"
+                + "| | | |  / _ \\  | |_) || | | |\n"
+                + "| |_| | / ___ \\ |  _ < | |_| |\n"
+                + "|____/ /_/   \\_\\|_| \\_\\ \\___/ \n";
+        System.out.println(banner);
+    }
+}
