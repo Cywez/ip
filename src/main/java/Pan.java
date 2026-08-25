ec4a7154b1c353d650356d0efaa410fd6ee0b626
Cywez <cherylchow04@gmail.com>
2026-08-25 13:32:34 +0800

    added mark as done

--- src/main/java/Pan.java
+++ src/main/java/Pan.java
@@ -19,6 +19,8 @@ public class Pan {
 
         Scanner scanner = new Scanner(System.in);
         String[] list = new String[100];
+        // Parallel array tracking done-state per task (no Task class, per project scope).
+        boolean[] done = new boolean[100];
         int count = 0;
         while (true) {
             String input = scanner.nextLine();
@@ -26,13 +28,27 @@ public class Pan {
                 break;
             }
             if (input.equals("list")) {
+                System.out.println(" Ooh ooh, here's what PanPan dug up for you~ PanPan's list-finding skills are Pan-tastic, teehee!!:");
                 for (int i = 0; i < count; i++) {
-                    System.out.println(" " + (i + 1) + ". " + list[i]);
+                    System.out.println(" " + (i + 1) + "." + (done[i] ? "[X]" : "[ ]") + " " + list[i]);
+                }
+            } else if (input.startsWith("mark ")) {
+                try {
+                    int index = Integer.parseInt(input.substring(5).trim()) - 1;
+                    if (index < 0 || index >= count) {
+                        System.out.println(" Ehh?? PanPan looked everywhere but that task number doesn't exist~ (｡•́︿•̀｡) PanPan is confused!!");
+                    } else {
+                        done[index] = true;
+                        System.out.println(" Yayyy!! PanPan marked this task as done, Pan-tastic job!!");
+                        System.out.println("   [X] " + list[index]);
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println(" Ooh wait wait~ PanPan needs a real task number, like \"mark 2\", okay?? PanPan believes in youuu!! (๑˃́ꇴ˂̀๑)");
                 }
             } else {
                 list[count] = input;
                 count++;
-                System.out.println(" added: " + input);
+                System.out.println(" PanPan added " + input + " to your list! PanPan gets bamboo treats? (´,,•ω•,,)");
             }
             System.out.println(LINE);
         }
