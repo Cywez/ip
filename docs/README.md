d5a2a61f3fce925c2c0017ec12490b1312aa5d4f
Cywez <cherylchow04@gmail.com>
2026-08-25 11:49:00 +0800

    Personalised greetings, Daru -> Pan)

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Daru User Guide
+# Pan User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Pan.java
@@ -0,0 +1,20 @@
+public class Pan {
+
+    private static final String LINE = "____________________________________________________________";
+
+    public static void main(String[] args) {
+        String banner = " ____      _     _   _ \n"
+                + "|  _ \\    / \\   | \\ | |\n"
+                + "| |_) |  / _ \\  |  \\| |\n"
+                + "|  __/  / ___ \\ | |\\  |\n"
+                + "|_|    /_/   \\_\\|_| \\_|\n";
+        System.out.println(LINE);
+        System.out.println(banner);
+        System.out.println("Heyyy hihi~ (๑>ᴗ<๑) It's meeeee, PanPan!!");
+        System.out.println("PanPan is SUPER happy you're here today, teehee~");
+        System.out.println("What can PanPan do for you todayy??");
+        System.out.println(LINE);
+        System.out.println("Byeee Byeee! PanPan will stay cute for you in the meantime! Mwah mwah~ (˘▾˘~)");
+        System.out.println(LINE);
+    }
+}
